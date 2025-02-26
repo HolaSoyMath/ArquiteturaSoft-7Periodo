35be4956c7f4e19a6040f3394c4b8f2995b23001
Matheus Santos <msantos@gocn.com.br>
2025-02-26 20:00:01 -0300

    update server response to include route handling and add package-lock.json to .gitignore

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ import http from "http"
 
 const PORT = 3000
 
+const rotas = {
+    "/": "API com Express e Node.js",
+    "/posts": "Rota de postagens",
+    "/autores": "Rota de autores",
+}
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, {"Content-type": "text-plain"});
-    res.end("API com Express e Node.js")
+    res.end(rotas[req.url])
 })
 
 server.listen(PORT, () => {
