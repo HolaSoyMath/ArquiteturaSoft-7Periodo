eaad6fec4cb99c51d3b509784f9eb3e46cc1e279
Davi Coene Rosa <davicoenerosa@gmail.com>
2025-02-26 21:57:41 -0300

    Iniciação do CRUD

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
 import express from "express";
 
 const app = express();
+app.use(express.json()); //middleware
+
+function searchPost(id){
+    return posts.findIndex(posts => {
+        return posts.id === Number(id)
+    });
+
+};
 
 const posts = [
     {
@@ -32,7 +40,31 @@ app.get("/posts", (req, res) => {
     res.status(200).json(posts);
 });
 
+app.post("/posts", (req, res) => {
+    posts.push(req.body);
+    res.status(201).send("Post criado com sucesso");
+});
 
+app.get("/posts/:id", (req, res) => {
+    const index = searchPost(req.params.id);
+    res.status(200).json(posts[index]);
+});
 
 export default app;
 
+app.put("/posts/:id", (req, res) => {
+    const index = searchPost(req.params.id);
+
+    posts[index].title = req.body.title 
+    posts[index].description = req.body.description
+    posts[index].author = req.body.author
+    res.status(200).json(posts[index]);
+
+})
+
+app.delete("/posts/:id", (req, res) => {
+    const index = searchPost(req.params.id);
+    posts.splice(index, 1);
+    res.status(200).send("Post removido COM SUCESSO!" );
+
+})
